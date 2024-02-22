716ed322fb6d32b72fdfe822e7aa3e80f9c2d2d4
Marcio Victor <marcio9769@gmail.com>
2024-02-22 20:24:54 -0300

    feat: criado pagina para usuario autenticado

--- ponto_eletronico/views.py
+++ ponto_eletronico/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Empresa, CustomUser
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
+
+from django.utils import timezone
 
 def home(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/homeAuthenticated  .html', {'usuario': request.user})
+        return render(request, 'usuarios/homeAuthenticated.html')
     else:
-        return render(request, 'usuarios/home.html')
+        if request.method == 'GET':
+            return render(request, 'usuarios/home.html')
+        if request.method == 'POST':
+            cpf = request.POST.get('cpf')
+            senha = request.POST.get('senha')
+
+            username = CustomUser.objects.get(cpf=cpf).username
+            password = senha
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                auth_login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'usuarios/home.html', {'error_message': 'Usuário ou senha inválidos!'})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
